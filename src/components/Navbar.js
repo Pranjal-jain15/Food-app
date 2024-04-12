bf9de9e656860429c7ac8e80d99c36e882c51511
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-12 13:19:29 -0600

    -

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,30 @@
 import { useState } from "react"
-
+import Sidebar from "./Sidebar"
 
 export default function Navbar() {
   const [showSidebar, setShowSidebar] = useState(false)
+  const links = [
+    {
+      name: 'Home',
+      path: '/'
+    },
+    {
+      name: 'recipes',
+      path: '/recipes'
+    },
+    {
+      name: 'settings',
+      path: "/settings"
+    }
+  ]
   return (
-    <div className="navbar container">
+    <><div className="navbar container">
       <a href="#!" className="logo">F<span>oo</span>diesHub</a>
       <div className="nav-links">
-        <a href="#!">Home</a>
+        {links.map(link)} 33333333333 start from here mittro
+        {/* <a href="#!">Home</a>
         <a href="#!">Recipes</a>
-        <a href="#!">Settings</a>
+        <a href="#!">Settings</a> */}
       </div>
       <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
         <div className="bar"></div>
@@ -17,5 +32,6 @@ export default function Navbar() {
         <div className="bar"></div>
       </div>
     </div>
+      <Sidebar /></>
   )
 } 
