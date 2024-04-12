caee1f3f25b02d50cbfb0b1711712cecb00c491f
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-12 12:09:35 -0600

    hustle is on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
-export default function Navbar(){
-    return(
-      <div className="navbar container">
-        <a  href="#!" className="logo">F<span>oo</span>diesHub</a>
-        <div className="nav-links">
-            <a  href="#!">Home</a>
-            <a  href="#!">Recipes</a>
-            <a  href="#!">Settings</a>
-        </div>
-        <div className="sidebar-btn">
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-      </div>  
-    )
+import { useState } from "react"
+
+
+export default function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false)
+  return (
+    <div className="navbar container">
+      <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+      <div className="nav-links">
+        <a href="#!">Home</a>
+        <a href="#!">Recipes</a>
+        <a href="#!">Settings</a>
+      </div>
+      <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </div>
+  )
 } 
