5f04164c5998108b7deb1841fc9757b2b40ffae8
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-12 11:35:38 -0600

    on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,15 @@ export default function Navbar(){
     return(
       <div className="navbar container">
         <a  href="#!" className="logo">F<span>oo</span>diesHub</a>
-        <div classname="nav-links">
-            <a  href="#!" >Home</a>
-            <a  href="#!" >Recipes</a>
-            <a  href="#!" >Settings</a>
+        <div className="nav-links">
+            <a  href="#!">Home</a>
+            <a  href="#!">Recipes</a>
+            <a  href="#!">Settings</a>
+        </div>
+        <div className="sidebar-btn">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
         </div>
       </div>  
     )
