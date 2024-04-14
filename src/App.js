4fe23c23dccfd2e1ec407701baa44d0715c526a0
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-14 12:41:01 -0600

    hourly update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar"
 import HeroSection from "./components/HeroSection";
-
+import ImproveSkills from "./components/ImproveSkills";
+import QouteSection from "./components/QuoteSection";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container main">
         <HeroSection />
+        <ImproveSkills />
+        <QouteSection />
       </div>
     </div>
   );
