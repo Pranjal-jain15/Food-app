cfa676167c28672ff974fa550307f468ee85aaac
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-14 11:50:19 -0600

    hourly commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar"
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+      <Navbar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
