66c17a0ec1fa7db230c008a08c138c71b4d695a0
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-14 13:15:42 -0600

    chef section in process

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import HeroSection from "./components/HeroSection";
 import ImproveSkills from "./components/ImproveSkills";
 import QouteSection from "./components/QuoteSection";
+import ChefsSection from "./components/ChefsSection";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <HeroSection />
         <ImproveSkills />
         <QouteSection />
+        <ChefsSection />
       </div>
     </div>
   );
