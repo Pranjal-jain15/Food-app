0d5fe86f0766a6a4614b1dfe26a5e383e91c7a68
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2024-04-15 11:57:43 -0600

    making sep pages
    --

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import Navbar from "./components/Navbar"
-import HeroSection from "./components/HeroSection";
-import ImproveSkills from "./components/ImproveSkills";
-import QouteSection from "./components/QuoteSection";
-import ChefsSection from "./components/ChefsSection";
+import Home from './pages/Home'
 import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container main">
-        <HeroSection />
-        <ImproveSkills />
-        <QouteSection />
-        <ChefsSection />
+        <Home />
       </div>
       <Footer />
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import HeroSection from "../components/HeroSection";
+import ImproveSkills from "../components/ImproveSkills";
+import QouteSection from "../components/QuoteSection";
+import ChefsSection from "../components/ChefsSection";
+
+export default function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <ImproveSkills />
+            <QouteSection />
+            <ChefsSection />
+        </div>
+    )
+}
